c680452b97ccfc410e099f8655f6a59cd35d5a43
Thercio <thercio01395@hotmail.com>
2016-02-01 16:14:55 -0200

    Carrinho de compras finalizado!

--- virtualStore/carrinho.php
+++ virtualStore/carrinho.php
@@ -53,7 +53,18 @@
 
 				//$meucarrinho[] = array('idproduto' => $idproduto, 'nome' => $nome, 'preco' => $preco, 'quantidade' => $quantidade);
 			}
-		} 
+		}
+
+		if (isset($_POST['id2'])) {
+			$indice = $_POST['id2'];
+			$quant = $_POST['quantidade2'];
+			$meucarrinho [$indice] ['quantidade'] = $quant;
+		}
+
+		if (isset($_POST['id3'])) {
+			$indice = $_POST['id3'];
+			$meucarrinho [$indice] = null;
+		}
 
 		if (isset ($meucarrinho)) $_SESSION['carrinho'] = $meucarrinho;
 	
@@ -73,15 +84,19 @@
 		if (isset($meucarrinho)) {
 			$total = 0;
 			for ($i=0; $i < count($meucarrinho) ; $i++) { 
+
+				if ($meucarrinho[$i]<>null) {
+					
 		?>
 		<tr class="tr4">
 			<td><?php printf($meucarrinho[$i] ['nome']); ?></td>
 			<td>R$ <?php printf($meucarrinho[$i] ['preco']); ?></td>
 			<td>
 				<form action="" method="post" name="atualizar">
-					<input name="id2" type="hidden" value="<?php printf($i)?>" />
+					<input name="id2" type="hidden" value="<?php printf($i); ?>" />
 					<input name="quantidade2" type="text" value="<?php printf($meucarrinho[$i] ['quantidade']); ?>" />
 					<input name="" type="image" src="imagens/atua.png" width="20" height="20"/>
+					<!--Quiser colocar um botão aqui, coloca name="id2" -->
 				</form>
 			</td>
 			<?php
@@ -89,9 +104,16 @@
 				$total = $total + $subtotal;
 			 ?>
 			<td style="background: #008800; color: #FFFFFF; font-weight: bold;">R$ <?php printf($subtotal); ?></td>
-			<td style="background: #FFF; border-top: 0;"><button class="btn btn-danger">Remover</button></td>
+			<td style="background: #FFF; border-top: 0;">
+				<form action="" method="post">
+					<input name="id3" type="hidden" value="<?php printf($i); ?>" />
+					<input name="" type="image" src="imagens/delete.png" width="50" height="50">
+					<!--Quiser colocar um botão aqui, coloca name="id3" -->
+				</form>
+			</td>
 		</tr>
 		<?php 
+			}
 			}
 		}
 		?>
@@ -102,17 +124,17 @@
 	</table>
 	<form action="" method="post">
 		<?php 
-			if (isset($_POST['fecha'])) {
+			if (isset($_POST['removeAll'])) {
 				session_destroy();
 			}
 		?>
 		<br />
-		<input name="fecha" type="submit" class="btn btn-danger btn-xs" value="Remover todos" />
+		<input name="removeAll" type="submit" class="btn btn-danger btn-xs" value="Remover todos" />
 	</form>
 	<!--
 				ASSISTIR A ESTA
 					VIDEO AULA
-				https://www.youtube.com/watch?v=ozT3fJXvOYM&index=32&list=PLxNM4ef1BpxikUFzfzEk-yRS5g7hmVZWx
+											https://www.youtube.com/watch?v=31PbBeswni0&list=PLxNM4ef1BpxikUFzfzEk-yRS5g7hmVZWx&index=35
 					DO COMEÇO
 				VLW!
 
